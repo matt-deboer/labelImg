662f0b1629799b4535c9971745154ce3fb212e4d
Ravindra Kompella <kmsravindra@yahoo.com>
2018-09-26 14:18:54 +0530

    Added back the PyQt4 libraries
    Added back the PyQt4 libraries

--- labelImg.py
+++ labelImg.py
@@ -12,19 +12,20 @@ import xml.etree.ElementTree as ET
 from functools import partial
 from collections import defaultdict
 
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-# except ImportError:
-#     # needed for py3+qt4
-#     # Ref:
-#     # http://pyqt.sourceforge.net/Docs/PyQt4/incompatible_apis.html
-#     # http://stackoverflow.com/questions/21217399/pyqt4-qtcore-qvariant-object-instead-of-a-string
-#     if sys.version_info.major >= 3:
-#         import sip
-#         sip.setapi('QVariant', 2)
-#     # from PyQt4.QtGui import *
-#     # from PyQt4.QtCore import *
+try:
+    from PyQt5.QtGui import *
+    from PyQt5.QtCore import *
+    from PyQt5.QtWidgets import *
+except ImportError:
+    # needed for py3+qt4
+    # Ref:
+    # http://pyqt.sourceforge.net/Docs/PyQt4/incompatible_apis.html
+    # http://stackoverflow.com/questions/21217399/pyqt4-qtcore-qvariant-object-instead-of-a-string
+    if sys.version_info.major >= 3:
+        import sip
+        sip.setapi('QVariant', 2)
+    from PyQt4.QtGui import *
+    from PyQt4.QtCore import *
 
 import resources
 # Add internal libs
